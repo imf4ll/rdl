490f7f6ef9a8d231baf70ed161e04dd7f9651a07
z3oxs <z3ox1s@protonmail.com>
2022-07-07 22:07:27 -0300

    feat: Added logger

---
+++ src/logger/mod.rs
@@ -0,0 +1,17 @@
+use std::process::exit;
+
+use colorism::{foreground::Fore, util::RESET};
+
+pub fn success(message: &str) {
+    println!("{}[✓]: {message}{RESET}", Fore::color(Fore::BdGreen));
+}
+
+pub fn warn(message: &str) {
+    println!("{}[⚠️]: {message}{RESET}\n", Fore::color(Fore::BdYellow));
+}
+
+pub fn error(message: &str) {
+    println!("{}[🚫]: {message}{RESET}", Fore::color(Fore::BdRed));
+
+    exit(3);
+}
